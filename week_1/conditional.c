0424a784e43362c35b360e5ccabce4be893c3947
Ben Hodge <benjamin.andrew.hodge@gmail.com>
2019-04-08 22:00:47 -0500

    initial commit

---
+++ week_1/conditional.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/* This is a comment */
+
+int main()
+{
+
+    int x = 1;
+    int y = 1;
+
+    if (x < y)
+    {
+        printf("X is less than y\n");
+    }
+    else if (x == y)
+    {
+        printf("x and y are the same\n");
+    }
+    else
+    {
+        printf("Y is less than x\n");
+    }
+
+    return 0;
+}
